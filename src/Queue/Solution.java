248e9ae26ce4c5458ce2715ba239d99f90871163
Ezechukwu Emmanuel <emereuwaonueze@gmail.com>
2022-05-05 10:26:33 +0100

    add more data structures

--- src/Queue/Solution.java
+++ src/Queue/Solution.java
@@ -1,19 +1,58 @@
 package Queue;
 
 public class Solution {
-    static class Node{
-        String val;
-        Node prev = null;
-        Node next = null;
-        public Node(String val){
-            this.val = val;
-        }
-
-    }
-
 
     static class Queue {
+        int[] queue;
+        int head, capacity, size;
+        public Queue(int k) {
+            queue = new int[k];
+            head = 0;
+            capacity = k;
+            size = 0;
+        }
 
+        public boolean enqueue(int value) {
+            if(size < capacity){
+                queue[(head + size) % capacity] = value;
+                size++;
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        public boolean dequeue() {
+            if(size > 0){
+                queue[head] = -1;
+                head = (head + 1) % capacity;
+                size--;
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        public int Front() {
+            if(size == 0) return -1;
+            return queue[head];
+        }
+
+        public int Rear() {
+            if(size == 0) return -1;
+            return queue[(head + size -1) % capacity];
+
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public boolean isFull() {
+            return size == queue.length;
+        }
     }
 
     public static void main(String[] args) {
@@ -28,7 +67,7 @@ public class Solution {
 
     private static void doTestsPass() throws Exception {
 
-        final Queue queue = new Queue();
+        final Queue queue = new Queue(3);
         // enqueue
     };
 
---
+++ src/Trie/Solution.java
@@ -0,0 +1,53 @@
+package Trie;
+
+public class Solution {
+    class TrieNode{
+        char c;
+        TrieNode[] children;
+        boolean isWord;
+
+        public TrieNode(char _c){
+            this.c = _c;
+            this.children = new TrieNode[26];
+        }
+    }
+
+    TrieNode root = new TrieNode(' ');;
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for(char c : word.toCharArray()){
+            if(node.children[c - 'a'] == null){
+                node.children[c - 'a'] = new TrieNode(c);
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(char c : word.toCharArray()){
+            if(node.children[c - 'a'] == null){
+                return false;
+            }else{
+                node = node.children[c - 'a'];
+            }
+        }
+
+        return node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(char c : prefix.toCharArray()){
+            if(node.children[c - 'a'] == null){
+                return false;
+            }else{
+                node = node.children[c - 'a'];
+            }
+        }
+
+        return true;
+    }
+}
