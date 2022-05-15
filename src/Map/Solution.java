5ca3050bb40317eacd50502374177a2def6528b2
Ezechukwu Emmanuel <emereuwaonueze@gmail.com>
2022-05-15 11:56:54 +0100

    add set and map datastructure

--- src/Map/Solution.java
+++ src/Map/Solution.java
@@ -1,4 +1,79 @@
 package Map;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
+    class Pair{
+        int first;
+        int second;
+
+        public Pair(int first, int second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+    class Bucket{
+        List<Pair> bucket = new LinkedList<>();
+
+        public void put(int key, int value) {
+            boolean found = false;
+            for(Pair pair : bucket){
+                if(pair.first == key){
+                    pair.second = value;
+                    found = true;
+                }
+            }
+            if(found == false){
+                bucket.add(new Pair(key,value));
+            }
+        }
+
+        public int get(int key) {
+            for(Pair pair : bucket){
+                if(pair.first == key){
+                    return pair.second;
+                }
+            }
+
+            return -1;
+        }
+
+        public void remove(int key) {
+            for(Pair pair : bucket){
+                if(pair.first == key){
+                    bucket.remove(pair);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    int hashKey;
+    List<Bucket> hashMap;
+
+    public Solution() {
+        hashKey = 2000;
+        hashMap = new ArrayList<>();
+        for(int i = 0; i < hashKey; i++){
+            hashMap.add(new Bucket());
+        }
+    }
+
+    public void put(int key, int value) {
+        int hashValue = key % hashKey;
+        hashMap.get(hashValue).put(key, value);
+    }
+
+    public int get(int key) {
+        int hashValue = key % hashKey;
+        return hashMap.get(hashValue).get(key);
+    }
+
+    public void remove(int key) {
+        int hashValue = key % hashKey;
+        hashMap.get(hashValue).remove(key);
+    }
 }
--- src/Set/Solution.java
+++ src/Set/Solution.java
@@ -1,4 +1,73 @@
 package Set;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
+
+    class Bucket{
+        List<Integer> bucket;
+        public Bucket(){
+            bucket = new LinkedList<>();
+        }
+        public void add(int key) {
+            if(contains(key)){
+                return;
+            }else{
+                bucket.add(key);
+            }
+        }
+        public void remove(int key) {
+            // Iterator iterator = bucket.iterator();
+            // while(iterator.hasNext()){
+            //     Integer next = (Integer)iterator.next();
+            //     if(next.equals(key)){
+            //         bucket.remove(next);
+            //         break;
+            //     }
+            // }
+            for(Integer value : bucket){
+                if(key == value){
+                    bucket.remove(value);
+                    break;
+                }
+            }
+        }
+        public boolean contains(int key) {
+            for(Integer value : bucket){
+                if(key == value){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+
+    int hashKey;
+    List<Bucket> hashSet;
+    public Solution() {
+        hashKey = 2000;
+        hashSet = new ArrayList<>();
+        for(int i = 0; i <= hashKey; i++){
+            hashSet.add(new Bucket());
+        }
+    }
+
+    public void add(int key) {
+        int hashValue = key % hashKey;
+        hashSet.get(hashValue).add(key);
+    }
+
+    public void remove(int key) {
+        int hashValue = key % hashKey;
+        hashSet.get(hashValue).remove(key);
+
+    }
+
+    public boolean contains(int key) {
+        int hashValue = key % hashKey;
+        return hashSet.get(hashValue).contains(key);
+    }
 }
