bf0ef80be48052d0b92b4f7540a1d7dc9ba82ba2
Ezechukwu Emmanuel <emereuwaonueze@gmail.com>
2022-06-27 17:04:02 +0100

    add quicksort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -4,9 +4,50 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-    public static int[] sort(int[] array){
+    public static int[] sort(int[] array) {
+        quickSortHelper(array, 0, array.length - 1);
+        return array;
+    }
 
-        return new int[]{};
+    public static void quickSortHelper(int[] array, int startIndex, int endIndex){
+        if(startIndex >= endIndex) return;
+
+        int pivotIndex = startIndex;
+        int leftIndex = startIndex + 1;
+        int rightIndex = endIndex;
+
+        while(leftIndex <= rightIndex){
+            if(array[leftIndex] > array[pivotIndex] && array[rightIndex] < array[pivotIndex]){
+                swap(array, leftIndex, rightIndex);
+            }
+
+            if(array[leftIndex] <= array[pivotIndex]){
+                leftIndex++;
+            }
+            if(array[rightIndex] >= array[pivotIndex]){
+                rightIndex--;
+            }
+        }
+
+        swap(array, pivotIndex, rightIndex);
+
+        // Improve the space complexity by sorting the smaller array first
+        boolean isLeftSmaller = rightIndex - 1 - startIndex < endIndex - (rightIndex + 1);
+        if(isLeftSmaller){
+            quickSortHelper(array, startIndex, rightIndex - 1);
+            quickSortHelper(array, rightIndex + 1, endIndex);
+        }else{
+            quickSortHelper(array, rightIndex + 1, endIndex);
+            quickSortHelper(array, startIndex, rightIndex - 1);
+
+        }
+
+    }
+
+    public static void swap(int[] array, int one, int two){
+        int temp = array[one];
+        array[one] = array[two];
+        array[two] = temp;
     }
 
     public static void main(String[] args) {
